ea7e38aee5e7c493f0be607d1811b79c03a8dffa
grvbrk <gouravbarikofficial@gmail.com>
2025-08-12 20:24:45 +0530

    added testcase models

---
+++ internal/models/testcase_model.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type TestCase struct {
+	ID        uuid.UUID `json:"id"`
+	ProblemID uuid.UUID `json:"problem_id"`
+	UI        string    `json:"ui"`
+	Input     string    `json:"input"`
+	Output    string    `json:"output"`
+	Position  int       `json:"position"`
+	IsActive  bool      `json:"is_active"`
+	CreatedAt time.Time `json:"created_at"`
+}
