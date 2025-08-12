445f6552576e5ccfe3f8bd48aef86cd4959ef4bb
grvbrk <gouravbarikofficial@gmail.com>
2025-08-12 20:25:58 +0530

    db.go and user store file

---
+++ internal/store/db.go
@@ -0,0 +1,47 @@
+package store
+
+import (
+	"database/sql"
+	"fmt"
+	"io/fs"
+	"os"
+
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/pressly/goose/v3"
+)
+
+func ConnectPGDB() (*sql.DB, error) {
+	db, err := sql.Open("pgx", os.Getenv("DB_URL"))
+	if err != nil {
+		return nil, fmt.Errorf("db: open %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("db: open %w", err)
+	}
+
+	fmt.Println("Connected to Database...")
+	return db, nil
+}
+
+func MigrateFS(db *sql.DB, migrationsFS fs.FS, dir string) error {
+	goose.SetBaseFS(migrationsFS)
+	defer func() {
+		goose.SetBaseFS(nil)
+	}()
+	return Migrate(db, dir)
+}
+
+func Migrate(db *sql.DB, dir string) error {
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("migrate: %w", err)
+	}
+
+	err = goose.Up(db, dir)
+	if err != nil {
+		return fmt.Errorf("goose up: %w", err)
+	}
+	return nil
+}
